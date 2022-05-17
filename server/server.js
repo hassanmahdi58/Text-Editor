87174cde8bab0568d49676b7b1b42b71f5d8625f
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-05-17 21:58:57 +0100

    make it better

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
@@ -9,10 +9,5 @@ app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-// app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
